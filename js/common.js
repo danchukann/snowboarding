5f4c11e173d0dfba4049f7b7ddaa37603dd9c927
danchukann <danchukann@gmail.com>
2017-04-27 17:12:50 +0300

    whole project

---
+++ js/common.js
@@ -0,0 +1,8 @@
+//$(document).ready(function(){
+ //       $('.ca-wrapper').bxSlider({
+   //       slideWidth: 200,
+   //       minSlides: 2,
+   //       maxSlides: 3,
+    //      slideMargin: 10
+    //    });
+    //  });
